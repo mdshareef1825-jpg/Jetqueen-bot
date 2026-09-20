484702c30b1a0e2ccac9aa92737a50325ee8c773
mdshareef1825-jpg <mdshareef1825@gmail.com>
2026-09-20 12:04:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
+import asyncio
 import os
-from pyrogram import Client, filters
+from pyrogram import Client
 
-# Render ke environment variables se API details lenge
-api_id = int(os.environ.get("31545473", 0))
-api_hash = os.environ.get("20e6f4ba06ca479f23a8b8b606888bc5", "")
+# Render ke environment variables se session string uthayega
+SESSION_STRING = os.environ.get("SESSION")
 
-# Client start hoga jo personal ID se message bheja karega
-app = Client("my_account_session", api_id=api_id, api_hash=api_hash)
+# Client initialize kar rahe hain
+app = Client(
+    "silpa_bot",
+    session_string=SESSION_STRING
+)
 
-@app.on_message(filters.private & filters.command("start"))
-def send_welcome(client, message):
-    # Yeh raha naya welcome message jo personal ID se jayega
-    welcome_text = "Hi dear , admin of JetQueen silpa Aviator here for you ❤️‍🔥\n\nAap aviator aur 1win khelte ho ❓❓❓"
-    message.reply_text(welcome_text)
+async def main():
+    async with app:
+        print("Bot successfully start ho gaya hai!")
+        # Silpa ke messages aur join requests handle karne ke liye bot active rahega
+        await asyncio.idle()
 
-print("Userbot script ready ho rahi hai...")
-app.run()
+if name == "__main__":
+    asyncio.run(main())
