ee7f4fc136cb1cde283faebbf761affca8298b31
mdshareef1825-jpg <mdshareef1825@gmail.com>
2026-09-21 16:22:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import asyncio
-import sys
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
 
-API_ID = 31545473  # Aapki API ID
-API_HASH = "20e6f4ba06ca479f23a8b8b606888bc5"  # Aapka API Hash
-SESSION_STRING = "BQHhWIEApyEut3QSUgybCvDehu-5UbMAXN2PkptDiVgeW_eRM0DYvHClkLb5Mf8mGvXieK4KevG5gn2_MDngl4sBD0Q-ZpEZUyUo7T_qU1kyg22eYfT7dwRdj1pNTPR9c8ZxMMtdCouVCGXjBrVN9JY_5G7tJU-sr0rSO5D8tEhC78Rql1ktvt6kcR2rQaR0f-7lsqhBtRgWu2ePq4BXWc2TVVLEQCdC1SIsj_UyPFbWIkQwq03oMPSMgZQPmnVVz5PMchAj0_GQ5NOF3hGH9QvN9hqYmGEyLWVI3Xos0X7kvTLMYVLHQKSNbiZ3OiTPSWxOwCk0VrSG9Veqv0C2U4ztr5PSqwAAAAITrWxxAA"  # Aapki Session String
+API_ID = 31545473
+API_HASH = "20e6f4ba06ca479f23a8b8b606888bc5"
+SESSION_STRING = "BQHhWIEApyEut3QSUgybCvDehu-5UbMAXN2PkptDiVgeW_eRM0DYvHClkLb5Mf8mGvXieK4KevG5gn2_MDngl4sBD0Q-ZpEZUyUo7T_qU1kyg22eYfT7dwRdj1pNTPR9c8ZxMMtdCouVCGXjBrVN9JY_5G7tJU-sr0rSO5D8tEhC78Rql1ktvt6kcR2rQaR0f-7lsqhBtRgWu2ePq4BXWc2TVVLEQCdC1SIsj_UyPFbWIkQwq03oMPSMgZQPmnVVz5PMchAj0_GQ5NOF3hGH9QvN9hqYmGEyLWVI3Xos0X7kvTLMYVLHQKSNbiZ3OiTPSWxOwCk0VrSG9Veqv0C2U4ztr5PSqwAAAAITrWxxAA"
 
 app = Client(
     "my_userbot",
@@ -31,16 +30,6 @@ async def welcome_message(client, message):
     except Exception as e:
         print(f"Error sending message: {e}")
 
-async def main():
-    print("Userbot is starting successfully...")
-    await app.start()
-    await asyncio.Event().wait()
-
 if name == "__main__":
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    
-    loop.run_until_complete(main())
+    print("Userbot is starting...")
+    app.run()
